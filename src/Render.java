7c49ec40c872d2ffc6cea48c433f6463b97d93c8
Josh Hofing <joshhofing@gmail.com>
2013-01-11 15:02:42 -0500

    Some methods that Render is going to need.

--- src/Render.java
+++ src/Render.java
@@ -1,9 +1,35 @@
+
+import java.awt.Color;
+
 public class Render {
     public Render (int width, int height) {
     }
 
     public Render () {
         this(100,100);
+    }
+
+
+
+    /**
+     * Render a color to a position
+     *
+     * This should draw to a buffer, which should be
+     * rendered with render()
+     *
+     * @arg x The x coordinate
+     * @arg y The y coordinate
+     * @arg c The color to draw
+     */
+    public void draw (int x, int y, Color c) {
+
+    }
+
+    /**
+     * Renders the stored buffer to the screen.
+     */
+    public void render () {
+        
     }
 
 }
