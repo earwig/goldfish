4c273a980566d08aa347fea703ec9d7120140e87
Josh Hofing <joshhofing@gmail.com>
2013-01-14 09:41:23 -0500

    RuleSet is now an interface, and Conway has placeholder code.

--- src/rules/Conway.java
+++ src/rules/Conway.java
@@ -1,9 +1,16 @@
 package edu.stuy.goldfish.rules;
 
+import edu.stuy.goldfish.Grid;
 import edu.stuy.goldfish.Patch;
-public class Conway extends RuleSet {
+
+public class Conway implements RuleSet {
 
     public Conway () {
         Patch p = new Patch();
+    }
+
+    @Override
+    public static Grid run (Grid g) {
+        return g; //TODO: do stuff.
     }
 }
--- src/rules/RuleSet.java
+++ src/rules/RuleSet.java
@@ -2,16 +2,16 @@ package edu.stuy.goldfish.rules;
 
 import edu.stuy.goldfish.Grid;
 
-public class RuleSet {
+public interface RuleSet {
 
     /**
      * Run this ruleset on a grid, returning the result.
      *
      * @param g The grid this is running on
      *
-     * @return 
+     * @return The new grid
      */
-    public static Grid run (Grid g) {
-        return g;
-    }
+    public static Grid run (Grid g);
+
+
 }
